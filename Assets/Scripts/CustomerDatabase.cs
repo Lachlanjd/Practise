a7d7f30b04fed379691439d7d4aa7dccf127de7b
Lachlan Dodds <lachlanjd@192-168-1-106.tpgi.com.au>
2020-04-25 14:04:39 +0800

    Practise

---
+++ Assets/Scripts/CustomerDatabase.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CustomerCustomClass
+{
+    public string firstName;
+    public string lastName;
+    public int age;
+    public string gender;
+    public string occupation;
+
+
+    public CustomerCustomClass(string firstName, string lastName, int age, string gender, string occupation)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.occupation = occupation;
+    }
+
+}
+
+
+public class CustomerDatabase : MonoBehaviour
+{
+    public CustomerCustomClass[] customers;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        customers[1] = new CustomerCustomClass("Joe", "Blow", 34, "Male", "Banker");
+        customers[2] = new CustomerCustomClass("Jess", "Winkle", 28, "Female", "Lawyer");
+        customers[3] = new CustomerCustomClass("John", "Smith", 52, "Male", "Waiter");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MoveToRandomPosition.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToRandomPosition : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3[] positions;
+    public int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions = CreateRandomPosition(5);
+        index = CreateRandomIndex(0, positions.Length);
+        player.transform.position = positions[index];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3[] CreateRandomPosition(int number)
+    {
+        Vector3[] pos = new Vector3[number];
+
+        for (int i = 0; i < number; i++)
+        {
+            pos[i] = new Vector3(Random.value * 50, Random.value * 50, Random.value * 50);
+        }
+
+        return pos;
+    }
+
+
+
+    int CreateRandomIndex(int min, int max)
+    {
+        int index = Random.Range(min, max);
+        return index;
+    }
+}
